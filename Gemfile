3f5da7f43ac6371abed42a18c8ecb4d8e904f92b
Deepali <deepali1.aconnexion@gmail.com>
2017-02-06 21:28:53 +0530

    Rspec added for testing.

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem 'request_store'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'rspec-rails', '~> 3.5'
 end
 
 group :development do
---
+++ spec/routing/dashboard_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "route to sign in customer", :type => :routing do
+  it "routes /dashboard to dashboards#show to show customer dashboard" do
+    expect(:get => "/dashboard").to route_to(
+      :controller => "dashboards",
+      :action => "show"
+    )
+  end
+
+end

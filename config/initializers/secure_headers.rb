d1a6900ebe25bc5920dc48ba3056868bc2b31e9d
Deepali <deepali1.aconnexion@gmail.com>
2017-04-13 15:23:07 +0530

    Secure_header gem added.

---
+++ config/initializers/secure_headers.rb
@@ -0,0 +1,16 @@
+SecureHeaders::Configuration.default do |config|
+  config.cookies = {
+    secure: ENV['APP_PROTOCOL'] == true, # mark all cookies as "Secure"
+    httponly: true, # mark all cookies as "HttpOnly"
+    samesite: {
+      lax: true # mark all cookies as SameSite=lax
+    }
+  }
+  config.csp.merge!(
+    default_src: %W(#{ENV['APP_PROTOCOL']}: 'self'),
+    img_src: %w('self' https://maps.googleapis.com https://csi.gstatic.com https://*.s3.amazonaws.com),
+    script_src: %w('self' https://maps.googleapis.com 'unsafe-eval'),
+    upgrade_insecure_requests: ENV['APP_PROTOCOL'] == true,
+    style_src: %w('unsafe-inline' 'self')
+  )
+end

deb21901229b4492a8801a9a24f51ecc87d0c229
Deepali <deepali1.aconnexion@gmail.com>
2017-04-19 15:18:39 +0530

    Minor bug.

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -6,7 +6,7 @@ class VenuesController < ApplicationController
     if params[:ll]
       venues = Venue.all(ll: params[:ll], query: params[:query])
     else
-      venues = Venue.all(query: params[:query], params['intent'])
+      venues = Venue.all(query: params[:query], intent: params[:intent])
     end
     render json: venues, each_serializer: VenueSerializer
   end

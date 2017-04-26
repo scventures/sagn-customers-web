455850d1f9a8b9f23ea958b290d36d1489454d18
Deepali <deepali1.aconnexion@gmail.com>
2017-04-26 15:56:27 +0530

    UI bugs.

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -9,7 +9,7 @@ SecureHeaders::Configuration.default do |config|
   config.csp.merge!(
     default_src: %W(#{ENV['APP_PROTOCOL']}: 'self'),
     img_src: %w('self' https://maps.googleapis.com https://csi.gstatic.com https://*.s3.amazonaws.com data: https://maps.gstatic.com),
-    script_src: %w('self' https://maps.googleapis.com https://js.stripe.com/v2/ ),
+    script_src: %w('self' https://maps.googleapis.com https://js.stripe.com/v2/ 'unsafe-eval'),
     upgrade_insecure_requests: ENV['APP_PROTOCOL'] == true,
     style_src: %w('unsafe-inline' 'self' https://fonts.googleapis.com/css)
   )

b602d3066f82fda95419198bf35721f396ea7b41
Deepali <deepali1.aconnexion@gmail.com>
2017-05-19 18:19:58 +0530

    Added child-src for stripe version 2.

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -12,7 +12,7 @@ SecureHeaders::Configuration.default do |config|
     script_src: %w('self' https://maps.googleapis.com https://js.stripe.com/v3/ https://api.stripe.com/v1/ https://cdn.layer.com/sdk/ 'unsafe-eval' 'unsafe-inline'),
     upgrade_insecure_requests: ENV['APP_PROTOCOL'] == true,
     style_src: %w('unsafe-inline' 'self' https://fonts.googleapis.com/css),
-    child_src: %w('self' https://js.stripe.com/v3/),
+    child_src: %w('self' https://js.stripe.com/v3/ https://js.stripe.com/v2/),
     connect_src: %w('self' https: wss:)
   )
 end

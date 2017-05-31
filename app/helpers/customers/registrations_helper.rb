1a1697673ccab9bc237e7edf633b85a76dd5d3dc
Deepali <deepali1.aconnexion@gmail.com>
2017-05-31 13:38:31 +0530

    Minor UI Bugs.

--- app/helpers/customers/registrations_helper.rb
+++ app/helpers/customers/registrations_helper.rb
@@ -1,8 +1,8 @@
 module Customers::RegistrationsHelper
 
   def tos_label
-    tos_link = link_to 'Terms of use', '#'
-    privacy_policy_link = link_to 'privacy Policy', '#'
+    tos_link = link_to 'Terms of use', page_path('terms_of_use')
+    privacy_policy_link = link_to 'privacy Policy', page_path('terms_of_use')
     "Accept #{tos_link} and #{privacy_policy_link}".html_safe
   end
 

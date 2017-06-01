9c6a4a795e49516225a1462213dad458ffe74331
Deepali <deepali1.aconnexion@gmail.com>
2017-06-01 16:11:48 +0530

    Current Request estimate page functionality and UI.

--- app/models/estimation.rb
+++ app/models/estimation.rb
@@ -3,5 +3,5 @@ class Estimation
   parse_root_in_json :estimation, format: :active_model_serializers
   collection_path 'customers/accounts/:account_id/service_requests/:service_request_id/estimations'
   belongs_to :service_request
-  
+  attributes :reason  
 end

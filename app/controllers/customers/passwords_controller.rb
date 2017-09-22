ba353509430daf90993378f0b99d28ce1270fbeb
Deepali <deepali1.aconnexion@gmail.com>
2017-09-22 20:13:48 +0530

    Skip render app filter for password controller.

--- app/controllers/customers/passwords_controller.rb
+++ app/controllers/customers/passwords_controller.rb
@@ -1,9 +1,19 @@
 class Customers::PasswordsController < Devise::PasswordsController
-
+  skip_before_action :render_getapps, only: [:edit, :update]
+  layout :password_layout
+  
   protected
+  
+  def password_layout
+    if browser.platform.android? or browser.platform.ios?
+      'mobile'
+    else
+      'application'
+    end
+  end
 
   def after_resetting_password_path_for(resource)
     new_customer_session_path
   end
-
+  
 end
